5fd303c30e2cd72cf6c4ea5b23adf9828cc44bae
Anton <Anton199515@yandex.ru>
2014-09-26 20:34:00 +0400

    test

---
+++ app/src/main/java/ru/ifmo/md/lesson3/ActivityForImage.java
@@ -0,0 +1,13 @@
+package ru.ifmo.md.lesson2;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityForImage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
